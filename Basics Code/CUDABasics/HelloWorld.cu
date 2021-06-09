e48d2116077b419128ff6820b4dd86d0292b37c4
arul2810 <arul2810@gmail.com>
2021-06-10 02:15:53 +0530

    Comments Update - helloworld
    Update to comments on hello world

--- Basics Code/CUDABasics/HelloWorld.cu
+++ Basics Code/CUDABasics/HelloWorld.cu
@@ -1,34 +1,53 @@
-﻿#include <stdio.h>
+﻿/*
+
+File Name : HelloWorld.cu
+
+File Description: Print Hello World using CPU and CPU
+
+File Author : Arul Prakash Samathuvamani - hello@arulprakash.dev - https://arulprakash.dev
+
+Date: 6/10/2021
+
+*/
+
+
+// Declare Headers
+
+#include <stdio.h>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-
+// Declaration of function to run on CPU
 void cpu_run_function() {
 
-	printf("This Function is run by the CPU \n");
+	printf("This Function is run by the CPU \n"); // This runs on CPU
 
 }
 
+// Declaration of function that is to be run on GPU.
 __global__ 
 void gpu_run_function() {
 
-	printf("This function is run by GPU \n");
+	printf("This function is run by GPU \n"); // This runs on GPU
 
 }
 
+// Main function, runs on CPU
 
 int main() {
-
+	 
 	printf("Running the function \n");
 
 
-	cpu_run_function();
+	cpu_run_function(); // Call CPU function
 
-	gpu_run_function <<<10, 10 >>> ();
+	gpu_run_function <<<10, 10 >>> (); // Call GPU function with 10 blocks each with 10 threads
 
-	cudaDeviceSynchronize();
+	cudaDeviceSynchronize(); // Wait for the process to finish
 
-	cpu_run_function();
+	cpu_run_function(); // Run CPU function again
+
+	// Replicate this function without cudaDeviceSynchronise()
 
 	return 0;
 }
