6db5d9eae27d566369d70feb6895a4fd5f40edb1
arul2810 <arul2810@gmail.com>
2021-06-10 01:57:30 +0530

    Update Hello CPU Program
    Hello GPU Program added

---
+++ Basics Code/CUDABasics/HelloWorld.cu
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+
+void cpu_run_function() {
+
+	printf("This Function is run by the CPU \n");
+
+}
+
+__global__ 
+void gpu_run_function() {
+
+	printf("This function is run by GPU \n");
+
+}
+
+
+int main() {
+
+	printf("Running the function \n");
+
+
+	cpu_run_function();
+
+	gpu_run_function <<<10, 10 >>> ();
+
+	cudaDeviceSynchronize();
+
+	cpu_run_function();
+
+	return 0;
+}
